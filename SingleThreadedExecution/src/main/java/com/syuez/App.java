c94e8bf78ccaeeca1e6bb93d5eea9950fc439bac
syuez <admin@syuez.com>
2022-11-22 14:01:30 +0800

    Single Threaded Execution Pattern

---
+++ SingleThreadedExecution/src/main/java/com/syuez/App.java
@@ -0,0 +1,13 @@
+package com.syuez;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
